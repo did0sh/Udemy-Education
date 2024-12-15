02ac11ba5b7dad36622ae93d2f8876804d272f38
Deyan Georgiev <deyan.p.georgiev@gmail.com>
2024-12-15 19:27:36 +0200

    Implementing the create new task functionality

--- Angular-The-Complete-Course/01-starting-project/src/app/new-task/new-task.component.ts
+++ Angular-The-Complete-Course/01-starting-project/src/app/new-task/new-task.component.ts
@@ -1,18 +1,36 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../user/user.model';
 import { Task } from '../tasks/task.model';
+import { FormsModule } from '@angular/forms';
+import { DUMMY_TASKS } from '../dummy/dummy-tasks';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancelAddTask = new EventEmitter<void>(); 
+  @Output() cancelAddTask = new EventEmitter<void>();
+  @Input({required:true}) user!: User;
+
+  titleInput = '';
+  summaryInput = '';
+  dueDateInput = '';
 
   closeTaskModal() {
     this.cancelAddTask.emit()
+  }
+
+  createTask() {
+    DUMMY_TASKS.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.user.id,
+      title: this.titleInput,
+      summary: this.summaryInput,
+      dueDate: this.dueDateInput
+    })
+    this.closeTaskModal();
   }
 }
